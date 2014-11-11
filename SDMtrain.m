9236ce3a653cef99145374bbeb59e8861d3be975
Daniel <danielsilva.ita@gmail.com>
2014-11-11 01:19:55 -0500

    base files

---
+++ SDMtrain.m
@@ -0,0 +1,16 @@
+function models = SDMtrain(mean_shape, annotations)
+% CV Fall 2014 - Provided Code
+% You need to implement the SDM training phase in this function, and
+% produce tracking models for Winnie the Pooh
+%
+% Input:
+%   mean_shape:    A provided 5x2 matrix indicating the x and y coordinates of 5 control points
+%   annotations:   A ground truth annotation for training images. Each row has the format
+%                  [frame_num nose_x nose_y left_eye_x left_eye_y right_eye_x right_eye_y right_ear_x right_ear_y left_ear_x left_ear_y]
+% Output:
+%   models:        The models that you will use in SDMtrack for tracking
+%
+
+% ADD YOUR CODE HERE
+	models = [];
+end
